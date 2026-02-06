5984113895123536e3c43cabfe67d0c9b93edd95
AjayAR <ajayperies10@gmail.com>
2026-02-06 22:48:34 +0530

    While Loops

--- Program.cs
+++ Program.cs
@@ -1,22 +1,13 @@
-﻿// Step 1: Ask for the name
-Console.WriteLine("Hello! What is your name?");
-string name = Console.ReadLine();
+﻿/* While Loops 
+int counter = 1; // 1. The Starting Point
 
-// Step 2: Ask for the age
-Console.WriteLine($"Hi {name}, how old are you?");
-string ageText = Console.ReadLine(); // We catch the text first
-
-// Step 3: The Translator (Text -> Number)
-int age = Convert.ToInt32(ageText);
-
-// Step 4: The Logic (If Statement)
-if (age >= 16)
+while (counter <= 5) // 2. The Condition (Keep going while this is true)
 {
-    Console.WriteLine("You are old enough to drive a car!");
+    Console.WriteLine($"Number: {counter}"); // 3. The Action
+    
+    counter = counter + 1; // 4. The Step (Moving forward)
 }
-else
-{
-    // Let's do some math to see how many years are left
-    int yearsLeft = 16 - age;
-    Console.WriteLine($"Sorry, you have to wait {yearsLeft} more years.");
-}
+
+Console.WriteLine("Finished!");
+*/
+
