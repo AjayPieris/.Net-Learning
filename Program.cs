64b1b68921aaf4599494662ef75fae231ba20435
AjayAR <ajayperies10@gmail.com>
2026-02-06 22:39:21 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Step 1: Ask for the name
+Console.WriteLine("Hello! What is your name?");
+string name = Console.ReadLine();
+
+// Step 2: Ask for the age
+Console.WriteLine($"Hi {name}, how old are you?");
+string ageText = Console.ReadLine(); // We catch the text first
+
+// Step 3: The Translator (Text -> Number)
+int age = Convert.ToInt32(ageText);
+
+// Step 4: The Logic (If Statement)
+if (age >= 16)
+{
+    Console.WriteLine("You are old enough to drive a car!");
+}
+else
+{
+    // Let's do some math to see how many years are left
+    int yearsLeft = 16 - age;
+    Console.WriteLine($"Sorry, you have to wait {yearsLeft} more years.");
+}
